bf26f64d475e48f04478ac0442c9c46bb7ca47d9
Tkex555 <bahamutthero@gmail.com>
2025-06-17 21:42:35 -0500

    se definio los atributos de la clase conexion

--- src/conexion/conexion.java
+++ src/conexion/conexion.java
@@ -1,5 +1,16 @@
 package conexion;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class conexion {
+	
+	private static final String URL = "jdbc:mysql://localhost:3306/Crud_PC";
+	private static final String Username = "root";
+	private static final String Password = "";
+	private static final String Driver = "com.mysql.cj.jdbc.Driver";
+	
+	
 	
 }
