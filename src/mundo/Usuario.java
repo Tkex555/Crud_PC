37b0ef9c6f3333e404dddd42bdf3f0b8d9a148cd
Samuel <rene28609@gmail.com>
2025-06-18 22:27:26 -0500

    agrege los atributos que tendria un usuario

--- src/mundo/Usuario.java
+++ src/mundo/Usuario.java
@@ -1,10 +1,20 @@
 package mundo;
 
+import java.util.Date;
+
 public class Usuario {
+	
+	private int id;
+	private Personas persona;
+	private String usuario;
+	private String passwordHash;
+	private String rol;
+	private Date fechaCreacion;
+	private Date ultimoAcceso;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 }
