067eff7ba5f7efbc75ff74441b42d507fb72e987
Samuel <rene28609@gmail.com>
2025-06-18 22:44:22 -0500

    agrege los atributos a la clase personas

--- src/mundo/Personas.java
+++ src/mundo/Personas.java
@@ -1,6 +1,24 @@
 package mundo;
 
+import java.util.Date;
+
 public class Personas {
+	
+	private int id;
+	private String primerNombre;
+	private String segundoNombre;
+	private String primerApellido;
+	private String segundoApellido;
+	private String email;
+	private String telefono;
+	private String direccion;
+	private String ciudad;
+	private String codigoPostal;
+	private String tipoDocumento;
+	private String numeroDocumento;
+	private Date fechaNacimiento;
+	private String genero;
+	private Date fechaRegistro;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
