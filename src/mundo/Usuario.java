7041e06a02c8f253ba84fad362c34dffcef67763
Samuel <rene28609@gmail.com>
2025-06-18 22:33:46 -0500

    agrege los setters de usuarios

--- src/mundo/Usuario.java
+++ src/mundo/Usuario.java
@@ -26,30 +26,57 @@ public class Usuario {
 		return id;
 	}
 	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
 	public Personas getPersona() {
 		return persona;
+	}
+	
+	public void setPersona(Personas persona) {
+		this.persona = persona;
 	}
 	
 	public String getUsuario() {
 		return usuario;
 	}
 	
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	
 	public String getPasswordHash() {
 		return passwordHash;
+	}
+	
+	public void setPasswordHash(String passwordHash) {
+		this.passwordHash = passwordHash;
 	}
 	
 	public String getRol() {
 		return rol;
 	}
 	
+	public void setRol(String rol) {
+		this.rol = rol;
+	}
+	
 	public Date getFechaCreacion() {
 		return fechaCreacion;
+	}
+	
+	public void setFechaCreacion(Date fechaCreacion) {
+		this.fechaCreacion = fechaCreacion;
 	}
 	
 	public Date getUltimoAcceso() {
 		return ultimoAcceso;
 	}
 	
+	public void setUltimoAcceso(Date ultimoAcceso) {
+		this.ultimoAcceso = ultimoAcceso;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
