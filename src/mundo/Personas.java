675b9054cd217c6147dd586eb44b00840a1fbb7d
Samuel <rene28609@gmail.com>
2025-06-18 22:48:19 -0500

    cree el constructor de personas segun lo previsto en la base de datos

--- src/mundo/Personas.java
+++ src/mundo/Personas.java
@@ -19,6 +19,27 @@ public class Personas {
 	private Date fechaNacimiento;
 	private String genero;
 	private Date fechaRegistro;
+	
+	public Personas( int id, String primerNombre, String segundoNombre, String primerApellido, String segundoApellido, 
+			String email, String telefono, String direccion, String ciudad, String codigoPostal, String tipoDocumento,
+			String numeroDocumento, Date fechaNacimiento, String genero, Date fechaRegistro) {
+		
+		this.id = id;
+		this.primerNombre = primerNombre;
+		this.segundoNombre = segundoNombre;
+		this.primerApellido = primerApellido;
+		this.segundoApellido = segundoApellido;
+		this.email = email;
+		this.telefono = telefono;
+		this.direccion = direccion;
+		this.ciudad = ciudad;
+		this.codigoPostal = codigoPostal;
+		this.tipoDocumento = tipoDocumento;
+		this.numeroDocumento = numeroDocumento;
+		this.fechaNacimiento = fechaNacimiento;
+		this.genero = genero;
+		this.fechaRegistro = fechaRegistro;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
