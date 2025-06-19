df81be0b6fec07d6ccac2379031e15aa4c022115
Tkex555 <bahamutthero@gmail.com>
2025-06-18 22:35:33 -0500

    se definio las variables

--- src/mundo/Producto.java
+++ src/mundo/Producto.java
@@ -4,7 +4,7 @@ public class Producto {
 
 	private int id;
 	private String nombre;
-	private Sting descripcion;
+	private String descripcion;
 	private double precio;
 	private int stock;
 	private String especicaiones_tecnicas;
@@ -13,5 +13,20 @@ public class Producto {
 	private Date fecha_creacion;
 	private Date fecha_actualizacion;
 	
+	public Producto(int id, String nombre, String modelo, String descripcion, 
+            double precio, int stock, String especificaciones_tecnicas, 
+            int id_categoria, int id_marca) 
+	{
+		this.id = id;
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+		this.precio = precio;
+		this.stock = stock;
+		this.especicaiones_tecnicas = especificaciones_tecnicas;
+		this.id_categoria = id_categoria;
+		this.id_marca = id_marca;
+		this.fecha_creacion = new Date();
+		this.fecha_actualizacion = new Date();
+	}
 	
 }
