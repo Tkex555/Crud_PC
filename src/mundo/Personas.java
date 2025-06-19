645b1b92ec088680a5f97196f35489a2a7426781
Samuel <rene28609@gmail.com>
2025-06-18 23:00:08 -0500

    agrege los setters del modelo personas

--- src/mundo/Personas.java
+++ src/mundo/Personas.java
@@ -46,60 +46,120 @@ public class Personas {
 		return id;
 	}
 	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
 	public String getPrimerNombre() {
 		return primerNombre;
+	}
+	
+	public void setPrimerNombre(String primerNombre) {
+		this.primerNombre = primerNombre;
 	}
 	
 	public String getSegundoNombre() {
 		return segundoNombre;
 	}
 	
+	public void setSegundoNombre(String segundoNombre) {
+		this.segundoNombre = segundoNombre;
+	}
+	
 	public String getPrimerApellido() {
 		return primerApellido;
+	}
+	
+	public void setPrimerApellido(String primerApellido) {
+		this.primerApellido = primerApellido;
 	}
 	
 	public String getSegundoApellido() {
 		return segundoApellido;
 	}
 	
+	public void setSegundoApellido(String segundoApellido) {
+		this.segundoApellido = segundoApellido;
+	}
+	
 	public String getEmail() {
 		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
 	}
 	
 	public String getTelefono() {
 		return telefono;
 	}
 	
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	
 	public String getDireccion() {
 		return direccion;
+	}
+	
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
 	}
 	
 	public String getCiudad() {
 		return ciudad;
 	}
 	
+	public void setCiudad(String ciudad) {
+		this.ciudad = ciudad;
+	}
+	
 	public String getCodigoPostal() {
 		return codigoPostal;
+	}
+	
+	public void setCodigoPostal(String codigoPostal) {
+		this.codigoPostal = codigoPostal;
 	}
 	
 	public String getTipoDocumento() {
 		return tipoDocumento;
 	}
 	
+	public void setTipoDocumento(String tipoDocumento) {
+		this.tipoDocumento = tipoDocumento;
+	}
+	
 	public String getNumeroDocumento() {
 		return numeroDocumento;
+	}
+	
+	public void setNumeroDocumento(String numeroDocumento) {
+		this.numeroDocumento = numeroDocumento;
 	}
 	
 	public Date getFechaNacimiento() {
 		return fechaNacimiento;
 	}
 	
+	public void setFechaNacimiento(Date fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+	
 	public String getGenero() {
 		return genero;
 	}
 	
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+	
 	public Date getFechaRegistro() {
 		return fechaRegistro;
+	}
+	
+	public void setFechaRegistro(Date fechaRegistro) {
+		this.fechaRegistro = fechaRegistro;
 	}
 	
 	
