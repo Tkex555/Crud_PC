59969fcdd353f4e6817d368fe14fb7eaaca4a9f3
Samuel <rene28609@gmail.com>
2025-06-18 22:29:07 -0500

    cree el constructor de usuario con los datos que tiene en la DB

--- src/mundo/Usuario.java
+++ src/mundo/Usuario.java
@@ -11,6 +11,16 @@ public class Usuario {
 	private String rol;
 	private Date fechaCreacion;
 	private Date ultimoAcceso;
+	
+	public Usuario(int id, Personas persona, String usuario, String passwordHash, String rol, Date fechaCreacion, Date ultimoAcceso) {
+		this.id = id;
+		this.persona = persona;
+		this.usuario = usuario;
+		this.passwordHash = passwordHash;
+		this.rol = rol;
+		this.fechaCreacion = fechaCreacion;
+		this.ultimoAcceso = ultimoAcceso;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
