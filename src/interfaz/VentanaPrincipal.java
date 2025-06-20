fb7791f71cfb3a213a7a2f1b0ddb99f78273661b
Tkex555 <bahamutthero@gmail.com>
2025-06-20 12:36:11 -0500

    se agrego que los botones sean mas delgados

--- src/interfaz/VentanaPrincipal.java
+++ src/interfaz/VentanaPrincipal.java
@@ -19,6 +19,8 @@ public class VentanaPrincipal extends JFrame {
         
         //botones
         JPanel panelBotones = new JPanel(new GringLayout(3, 1, 10, 10));
+        panelBotones.setBackground(new Color(230, 230, 250));
+        panelBotones.setBorder(BorderFactory.createEmptyBorder(20, 10, 20, 10));
 
         // Panel principal de productos
         PanelProductos panelProductos = new PanelProductos();
