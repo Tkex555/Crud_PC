425cd51a0300b63e7d8dfae5760fb1a54d20a40a
Samuel <rene28609@gmail.com>
2025-06-19 22:38:05 -0500

    realizo la comprobacion de la conexion

--- src/DAO/ProductoDAOimpl.java
+++ src/DAO/ProductoDAOimpl.java
@@ -1,5 +1,17 @@
 package DAO;
 
-public class ProductoDAOimpl {
+import java.sql.Connection;
 
+import conexion.conexion;
+
+public class ProductoDAOimpl implements ProductoDAO {
+	
+	private Connection conn;
+
+    public ProductoDAOimpl() {
+        this.conn = conexion.getConnection();
+        if (this.conn == null) {
+            System.out.println("La conexión es nula. Verifica los datos.");
+        }
+    }
 }
