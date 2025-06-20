d78f8e61a07f067ae0e0ab100d8fc93e41b8c155
Tkex555 <bahamutthero@gmail.com>
2025-06-19 21:38:11 -0500

    se agrego nombre

--- src/mundo/Categoria.java
+++ src/mundo/Categoria.java
@@ -25,5 +25,15 @@ public class Categoria {
 	{
 		this.id = id;
 	}
+	
+	public String getNombre() 
+	{
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) 
+	{
+		this.nombre = nombre;
+	}
 
 }
