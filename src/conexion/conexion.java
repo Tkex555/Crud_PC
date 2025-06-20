9e9a343ad10fd491722d4620cbd5a5ca4ed1e290
Samuel <rene28609@gmail.com>
2025-06-20 07:47:59 -0500

    agrege el driver para manejar la base de datos

--- src/conexion/conexion.java
+++ src/conexion/conexion.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class conexion {
 	
-	private static final String URL = "jdbc:mysql://localhost:3306/Crud_PC";
+	private static final String URL = "jdbc:mysql://localhost:3306/crud_pc";
 	private static final String Username = "root";
 	private static final String Password = "";
 	private static final String Driver = "com.mysql.cj.jdbc.Driver";
