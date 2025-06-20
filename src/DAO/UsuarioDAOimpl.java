620e458c4b7b975c7820009c755956e77e7b88ed
Samuel <rene28609@gmail.com>
2025-06-19 22:16:45 -0500

    cambio de unas cosas

--- src/DAO/UsuarioDAOimpl.java
+++ src/DAO/UsuarioDAOimpl.java
@@ -18,17 +18,6 @@ public class UsuarioDAOimpl implements UsuarioDAO {
 
     @Override
     public void registrar(Usuario usuario) {
-        String sql = "INSERT INTO usuarios (id_persona, usuario, password_hash, rol) VALUES (?, ?, ?, ?)";
-
-        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
-            stmt.setInt(1, usuario.getPersona().getId());
-            stmt.setString(2, usuario.getUsuario());
-            stmt.setString(3, usuario.getContraseña());
-            stmt.setString(4, usuario.getRol());
-            stmt.executeUpdate();
-        } catch (SQLException e) {
-            System.out.println("Error al registrar usuario: " + e.getMessage());
-        }
     }
 
 	@Override
