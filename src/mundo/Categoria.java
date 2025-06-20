391fdf3c0a02d33f00251f2152f5176c89082129
Tkex555 <bahamutthero@gmail.com>
2025-06-19 21:37:32 -0500

    se agrego id

--- src/mundo/Categoria.java
+++ src/mundo/Categoria.java
@@ -14,5 +14,16 @@ public class Categoria {
 		this.fecha_creacion = fecha_creacion;
 		this.descripcion = descripcion;
 	}
+	
+	
+	public int getId() 
+	{
+		return id;
+	}
+	
+	public void setId(int id) 
+	{
+		this.id = id;
+	}
 
 }
