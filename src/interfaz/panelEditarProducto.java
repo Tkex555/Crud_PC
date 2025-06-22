fb3de286f48c5f10da516f0397a8a2d038e51aa1
Samuel <rene28609@gmail.com>
2025-06-21 21:46:03 -0500

    realizo las importaciones de las cosas que voy a utilizar

--- src/interfaz/panelEditarProducto.java
+++ src/interfaz/panelEditarProducto.java
@@ -1,5 +1,16 @@
 package interfaz;
 
+import DAO.ProductoDAO;
+import DAO.ProductoDAOimpl;
+import mundo.Producto;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.*;
+
+
 public class panelEditarProducto {
 
 }
