fa68251199708217454dc62e131d980ca508a41a
Samuel <rene28609@gmail.com>
2025-06-24 07:13:44 -0500

    java docs de categoriaDAO

--- src/DAO/CategoriaDAO.java
+++ src/DAO/CategoriaDAO.java
@@ -5,8 +5,18 @@ import java.util.ArrayList;
 import java.util.List;
 import mundo.Categoria;
 
+/**
+ * Clase que maneja las operaciones de acceso a datos para las categorías.
+ * Contiene métodos para obtener todas las categorías de la base de datos.
+ * Esta clase se conecta a una base de datos MySQL y ejecuta consultas SQL
+ */
 public class CategoriaDAO {
     // Devuelve todas las categorías de la base de datos
+	/**
+	 * Método para obtener todas las categorías de la base de datos.
+	 * Este método establece una conexión con la base de datos, ejecuta una consulta SQL
+	 * @return List<Categoria> - Lista de todas las categorías disponibles en la base de datos.
+	 */
     public List<Categoria> obtenerTodas() {
         List<Categoria> lista = new ArrayList<>();
         try (Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/crud_pc", "root", "");
@@ -28,7 +38,7 @@ public class CategoriaDAO {
     }
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
