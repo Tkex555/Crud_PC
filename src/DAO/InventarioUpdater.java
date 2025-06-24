09f6064de8a05f4388417aede8fc426af2831fe3
Samuel <rene28609@gmail.com>
2025-06-24 07:15:59 -0500

    mas documentacion

--- src/DAO/InventarioUpdater.java
+++ src/DAO/InventarioUpdater.java
@@ -5,8 +5,20 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import conexion.conexion;
 
+/**
+ * Clase que maneja la actualización del inventario de productos.
+ * Contiene métodos para actualizar los valores de inventario según el stock del producto.
+ * Esta clase se conecta a una base de datos MySQL y ejecuta consultas SQL.
+ */
 public class InventarioUpdater {
     // Lógica para actualizar los valores de inventario según el stock del producto
+		/**
+	 * Método para actualizar el inventario de un producto específico.
+	 * Este método establece una conexión con la base de datos, obtiene el stock actual,
+	 * el stock mínimo y máximo del producto, y actualiza estos valores según el nuevo stock proporcionado.
+	 * @param idProducto - Identificador único del producto cuyo inventario se va a actualizar.
+	 * @param nuevoStock - Nuevo valor de stock que se desea establecer para el producto.
+	 */
     public static void actualizarInventarioPorProducto(int idProducto, int nuevoStock) {
         try (Connection con = conexion.getConnection();
              PreparedStatement ps = con.prepareStatement("SELECT stock_actual, stock_minimo, stock_maximo FROM inventario WHERE id_producto = ?");) {
